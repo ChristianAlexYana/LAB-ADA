7bba018c2b0d48d3282e5329287c0d1664731040
Christian Yana <cyanahu@unsa.edu.pe>
2025-09-18 20:33:51 -0500

    agregamos el primer bucle de SelectionFloat

--- SelectionFloat.java
+++ SelectionFloat.java
@@ -1,3 +1,10 @@
 public class SelectionFloat {
-    
+    public static void main (String args[]){
+        float arr []= {5.12f ,2.15f ,5.14f ,2.11f ,6} ;
+        // primer bucle recorre el arreglo de inicio a penultimo
+        for(int i = 0; i < arr.length; i++){
+            int min = i; // se asume que el elemento actual es el mínimo
+        
+        }
+    }
 }
