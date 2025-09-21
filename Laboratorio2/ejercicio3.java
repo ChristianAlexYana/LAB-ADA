564da8479ea5c659266d0b2f366df96e3425c986
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-09-21 10:48:41 -0500

    colocando los metodos de ordenamiento

---
+++ Laboratorio2/ejercicio3.java
@@ -0,0 +1,33 @@
+package Laboratorio2;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        
+    }
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Intercambio
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+}
