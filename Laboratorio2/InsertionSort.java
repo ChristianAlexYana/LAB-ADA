24fad2770f69e34dae1fdce2c8ec7aae1aa70651
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-09-20 21:57:20 -0500

    probando los tiempos de ejecucion con distintos tipos de listas

--- Laboratorio2/InsertionSort.java
+++ Laboratorio2/InsertionSort.java
@@ -2,24 +2,45 @@ package Laboratorio2;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        String nombres[] = {"chritian", "ana", "maria", "zulema", "juan", "pedro", "alberto", "carlos", "beto"};
-        long inicio= System.nanoTime();
-        int pasos=0;
-        for(int i = 0; i < nombres.length - 1; i++){
-            for(int j = i + 1; j < nombres.length; j++){
-                if(nombres[i].compareTo(nombres[j]) > 0){ 
-                    String aux = nombres[i];
-                    nombres[i] = nombres[j];
-                    nombres[j] = aux;
-                    pasos++;
+        String nombresAleatorio[] = {"chritian", "ana", "maria", "zulema", "juan", "pedro", "alberto", "carlos", "beto"};
+        String nombresOrdenado[] = { "ana", "alberto", "beto", "carlos", "chritian", "juan", "maria", "pedro", "zulema" };
+        String nombresInverso[] = { "zulema", "pedro", "maria", "juan", "chritian", "carlos", "beto", "alberto", "ana" };
+        long inicio1= System.nanoTime();
+        ordenar(nombresAleatorio);
+        long fin1 = System.nanoTime();
+        long inicio2= System.nanoTime();
+        ordenar(nombresOrdenado);
+        long fin2 = System.nanoTime();
+        long inicio3= System.nanoTime();
+        ordenar(nombresInverso);
+        long fin3 = System.nanoTime();
+        System.out.println("Arreglo ordenado aleatorio: ");
+        imprimir(nombresAleatorio);
+        System.out.println("Tiempo de ejecucion para arreglo aleatorio: " + (fin1 - inicio1) + " nanosegundos");
+        System.out.println("Arreglo ordenado: ");
+        imprimir(nombresOrdenado);
+        System.out.println("Tiempo de ejecucion para arreglo ordenado: " + (fin2 - inicio2) + " nanosegundos");
+        System.out.println("Arreglo ordenado inverso: ");
+        imprimir(nombresInverso);
+        System.out.println("Tiempo de ejecucion para arreglo inverso: " + (fin3 - inicio3) + " nanosegundos");
+
+    }
+    public static void ordenar(String[] arreglo){
+        for(int i = 0; i < arreglo.length - 1; i++){
+            for(int j = i + 1; j < arreglo.length; j++){
+                if(arreglo[i].compareTo(arreglo[j]) > 0){ 
+                    String aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
                 }
             }
         }
-        long fin = System.nanoTime();
-        for(int i = 0; i < nombres.length; i++){
-            System.out.print(nombres[i] + " ");
-        }
-        System.out.println("\nTiempo de ejecucion: " + (fin - inicio) + " nanosegundos");
-        System.out.println("Pasos: " + pasos);
     }
+    public static void imprimir(String[] arreglo){
+        for(int i = 0; i < arreglo.length; i++){
+            System.out.print(arreglo[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
